659ac8f50fbe564d4e9295099681fac9423e45a9
Robinson C <robin@wizroots.com>
2022-05-19 15:30:51 +0530

    Nodemailer Email config

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+const port  = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Nodemailer is listening at http://localhost:${port}`);
+});
+
+
+app.post('/send-email', function (req, res) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+          clientId: process.env.OAUTH_CLIENTID,
+          clientSecret: process.env.OAUTH_CLIENT_SECRET,
+          refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        }
+    });
+
+    // console.log(req.body);
+    let mailOptions = {
+        from:'wizroots@gmail.com',
+        to: req.body.to, // list of receivers
+        subject: req.body.subject, // Subject line
+        text: req.body.text, // plain text body
+        // html: '<b>NodeJS Email Tutorial</b>' // html body
+    };
+
+    transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+          console.log("Error " + err);
+        } else {
+          console.log("Email sent successfully");
+        }
+        
+    });
+
+    res.end('{"success" : "Email sent successfully", "status" : 200}');
+
+});
