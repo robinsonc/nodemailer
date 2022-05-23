b2c8444a02f84140c370ab9efbde9c8c8b5c82ac
Robinson C <robin@wizroots.com>
2022-05-23 13:05:48 +0530

    CORS enabled

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port  = process.env.PORT;
 
 app.listen(port, () => {
